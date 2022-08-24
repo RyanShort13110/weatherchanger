f5093cb5fd223f8d50a0f8d08bfa9f0004ee103d
Ryan Short <110632551+RyanShort13110@users.noreply.github.com>
2022-08-24 12:21:56 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
-document.getElementById('sunny').onclick = sunParty
+document.getElementById('sunny').onclick = sunParty;
 
 function sunParty() {
-  document.querySelector('body').style.background = "url('sunny.gif')"
-  document.body.style.backgroundRepeat = "no-repeat"
-  document.body.style.backgroundPosition = "center"
-  document.body.style.backgroundSize = "100%"
-}
+  document.querySelector('body').style.background = "url('sunny.gif')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundSize = "100%";
+};
 
-document.getElementById('rainy').onclick = rainParty
+document.getElementById('rainy').onclick = rainParty;
 
 function rainParty() {
-document.querySelector('body').style.background = "url('rain.gif')"
-document.body.style.backgroundRepeat = "no-repeat"
-document.body.style.backgroundPosition = "center"
-document.body.style.backgroundSize = "100%"
-}
+document.querySelector('body').style.background = "url('rain.gif')";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundPosition = "center";
+document.body.style.backgroundSize = "100%";
+};
 
-document.getElementById('snowy').onclick = snowParty
+document.getElementById('snowy').onclick = snowParty;
 
 function snowParty() {
-  document.querySelector('body').style.background = "url('snow.gif')"
-  document.body.style.backgroundRepeat= "no-repeat"
-  document.body.style.backgroundPosition = "center"
-  document.body.style.backgroundSize = "100%"
-  img.style.display = block
-}
+  document.querySelector('body').style.background = "url('snow.gif')";
+  document.body.style.backgroundRepeat= "no-repeat";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundSize = "100%";
+  img.style.display = block;
+};
 
-document.getElementById('return').onclick = returnBack
+document.getElementById('return').onclick = returnBack;
 
 function returnBack() {
-  document.querySelector('body').style.background = "rgba(0,0,0,0)"
-  document.body.style.backgroundRepeat= "no-repeat"
-  document.body.style.backgroundPosition = "center"
-  document.body.style.backgroundSize = "100%"
-}
+  document.querySelector('body').style.background = "rgba(0,0,0,0)";
+  document.body.style.backgroundRepeat= "no-repeat";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundSize = "100%";
+};
